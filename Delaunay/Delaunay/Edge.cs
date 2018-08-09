311d7574c687f4a4ec8dd745668caa8204b10394
sehta7@gmail.com <sehta7@gmail.com>
2018-08-09 16:24:47 +0200

    add method to check if two edges are the same

--- Delaunay/Delaunay/Edge.cs
+++ Delaunay/Delaunay/Edge.cs
@@ -22,5 +22,18 @@ namespace Delaunay
             this.p1 = p1;
             this.p2 = p2;
         }
+
+        //checks if two edges are the same
+        public static bool areTheSame(Edge e1, Edge e2)
+        {
+            bool same = false;
+
+            if (e1.p1 == e2.p2 && e1.p2 == e2.p1 || e1.p1 == e2.p1 && e1.p2 ==e2.p2)
+            {
+                same = true;
+            }
+
+            return same;
+        }
     }
 }
