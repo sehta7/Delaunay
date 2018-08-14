6bb20bd04d7423b3c8214c24d7a752092d19800a
sehta7@gmail.com <sehta7@gmail.com>
2018-08-14 12:07:19 +0200

    first attempt to count edges of Voronoi diagram - unsuccessful

--- Delaunay/Delaunay/Voronoi_Service.cs
+++ Delaunay/Delaunay/Voronoi_Service.cs
@@ -11,11 +11,25 @@ namespace Delaunay
     {
         //method to get Voronoi diagram from Delaunay Triangulation
         //returns bisectors of triangles which have circumcenter
-        public static List<Edge> DelaunayToVoronoi(List<PointF> circumcentersList, List<Triangle> trianglesList)
+        public static List<Edge> DelaunayToVoronoi(List<Triangle> trianglesList)
         {
+            //list of Voronoi diagram edges
             List<Edge> edgeList = new List<Edge>();
+            List<float> bisectorList = new List<float>();
 
-
+            foreach (var triangle in trianglesList)
+            {
+                for (int i = 0; i < trianglesList.Count; i++)
+                {
+                    float x, y;
+                    if (triangle.a1 != trianglesList[i].a1)
+                    {
+                        Geometry_Service.intersectPoint(triangle.a1, triangle.b1, trianglesList[i].a1, trianglesList[i].b1, out x, out y);
+                        PointF intersect = new PointF(x, y);
+                        edgeList.Add(new Edge(intersect, triangle.circumcenter));
+                    }
+                }
+            }
 
             return edgeList;
         }
