5d719c9341f09c71bb88565671904c635d9b93d7
sehta7@gmail.com <sehta7@gmail.com>
2018-08-14 10:32:28 +0200

    add class to make Voronoi diagram

--- Delaunay/Delaunay/Form1.cs
+++ Delaunay/Delaunay/Form1.cs
@@ -37,5 +37,10 @@ namespace Delaunay
             triangleList = Delaunay_Service.BowyerWatsonAlgorithm(pointList);
             Draw_Service.drawTriangle(bitmap, graphics, pictureBox1, triangleList);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Delaunay/Delaunay/Voronoi_Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delaunay
+{
+    class Voronoi_Service
+    {
+        //method to get Voronoi diagram from Delaunay Triangulation
+        //returns bisectors of triangles which have circumcenter
+        public static List<Edge> DelaunayToVoronoi(List<PointF> circumcentersList, List<Triangle> trianglesList)
+        {
+            List<Edge> edgeList = new List<Edge>();
+
+
+
+            return edgeList;
+        }
+    }
+}
