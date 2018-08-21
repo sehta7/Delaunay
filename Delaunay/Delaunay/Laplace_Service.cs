076c7c2d3eff3a01dc119ccd067a350d09dc9b0e
sehta7@gmail.com <sehta7@gmail.com>
2018-08-21 11:32:12 +0200

    new class to make Laplace smoothing

---
+++ Delaunay/Delaunay/Laplace_Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delaunay
+{
+    class Laplace_Service
+    {
+        //move vertices of triangles to smooth grid
+        public static List<Triangle> Laplace(List<Triangle> triangleList)
+        {
+            foreach (var triangle in triangleList)
+            {
+                //new coordinates of triangle vertex
+                float x = 0, y = 0;
+
+                //first vertex of triangle
+                Geometry_Service.movingTriangle(triangle.p1, triangleList, ref x, ref y);
+                triangle.p1.X = x;
+                triangle.p1.Y = y;
+
+                //first vertex of triangle
+                Geometry_Service.movingTriangle(triangle.p2, triangleList, ref x, ref y);
+                triangle.p2.X = x;
+                triangle.p2.Y = y;
+
+                //first vertex of triangle
+                Geometry_Service.movingTriangle(triangle.p3, triangleList, ref x, ref y);
+                triangle.p3.X = x;
+                triangle.p3.Y = y;
+            }
+
+            return triangleList;
+        }
+    }
+}
