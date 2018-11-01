3722b652a951c91a641ff7d0615ff980de4d6dec
sehta7@gmail.com <sehta7@gmail.com>
2018-11-01 17:30:22 +0100

    add method to count 2d values of 3d points

--- Delaunay/Delaunay/Model3D/Vector3D.cs
+++ Delaunay/Delaunay/Model3D/Vector3D.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,16 @@ namespace Delaunay.Model3D
             this.x = x;
             this.y = y;
             this.z = z;
+        }
+
+        public PointF Count2d(Camera camera, float zoom)
+        {
+            PointF point = new PointF();
+            float zValue = -this.z - camera.position.z;
+            point.X = ((camera.position.x - this.x) / zValue * zoom);
+            point.Y = ((camera.position.y - this.y) / zValue * zoom);
+
+            return point;
         }
     }
 }
