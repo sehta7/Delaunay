4362a610c7516ca2ddd69d1e374ac9df2160c503
sehta7@gmail.com <sehta7@gmail.com>
2018-11-12 18:41:25 +0100

    add rotation to 3d points

--- Delaunay/Delaunay/Model3D/Vector3D.cs
+++ Delaunay/Delaunay/Model3D/Vector3D.cs
@@ -20,14 +20,50 @@ namespace Delaunay.Model3D
             this.z = z;
         }
 
-        public PointF Count2d(Camera camera, float zoom)
+        public PointF Count2d(Camera camera, float zoom, PointF drawCenter)
         {
             PointF point = new PointF();
             float zValue = -this.z - camera.position.z;
-            point.X = ((camera.position.x - this.x) / zValue * zoom);
-            point.Y = ((camera.position.y - this.y) / zValue * zoom);
+            point.X = ((camera.position.x - this.x) / zValue * zoom) + drawCenter.X;
+            point.Y = ((camera.position.y - this.y) / zValue * zoom) + drawCenter.Y;
 
             return point;
+        }
+
+        public void xRotation(float degrees)
+        {
+            float cDegrees = degrees * (float)(Math.PI / 180.0);
+            float cosDegrees = (float)Math.Cos(cDegrees);
+            float sinDegrees = (float)Math.Sin(cDegrees);
+
+            float temp = y;
+            float tempZ = z;
+            y = (temp * cosDegrees) + (tempZ * sinDegrees);
+            z = (temp * -sinDegrees) + (tempZ * cosDegrees);
+        }
+
+        public void yRotation(float degrees)
+        {
+            float cDegrees = degrees * (float)(Math.PI / 180.0);
+            float cosDegrees = (float)Math.Cos(cDegrees);
+            float sinDegrees = (float)Math.Sin(cDegrees);
+
+            float temp = x;
+            float tempZ = z;
+            x = (temp * cosDegrees) + (tempZ * sinDegrees);
+            z = (temp * -sinDegrees) + (tempZ * cosDegrees);
+        }
+
+        public void zRotation(float degrees)
+        {
+            float cDegrees = degrees * (float)(Math.PI / 180.0);
+            float cosDegrees = (float)Math.Cos(cDegrees);
+            float sinDegrees = (float)Math.Sin(cDegrees);
+
+            float temp = x;
+            float tempY = y;
+            x = (temp * cosDegrees) + (tempY * sinDegrees);
+            y = (temp * -sinDegrees) + (tempY * cosDegrees);
         }
     }
 }
