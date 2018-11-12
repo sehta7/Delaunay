7cd5e73381cfe2d251ff31b8a19a1d3b6cc3cb05
sehta7@gmail.com <sehta7@gmail.com>
2018-11-12 18:47:19 +0100

    add option to make Voronoi diagram with random points

--- Delaunay/Delaunay/Model3D/Form3D.Designer.cs
+++ Delaunay/Delaunay/Model3D/Form3D.Designer.cs
@@ -36,6 +36,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,7 +45,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(708, 457);
+            this.pictureBox1.Size = new System.Drawing.Size(700, 460);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -112,11 +114,33 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(797, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 43);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(405, 488);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Delaunay";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form3D
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 523);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -144,5 +168,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Delaunay/Delaunay/Model3D/Form3D.cs
+++ Delaunay/Delaunay/Model3D/Form3D.cs
@@ -16,7 +16,11 @@ namespace Delaunay.Model3D
         Bitmap bitmap3d;
         float zoom;
         List<Vector3D> points3d;
+        List<PointF> points2d;
         Cube cube;
+        List<Triangle> trianglesXY;
+        List<Triangle> trianglesYZ;
+        List<Triangle> trianglesZX;
 
         public Form3D()
         {
@@ -45,7 +49,22 @@ namespace Delaunay.Model3D
 
         private void button1_Click(object sender, EventArgs e)
         {
-            points3d = Drawing_Service.randomPoints(bitmap3d, graphics3d, pictureBox1, Int32.Parse(textBox1.Text), pictureBox1.Size.Width, pictureBox1.Size.Height, 200);
+            points2d = new List<PointF>();
+            int minX, maxX, minY, maxY, minZ, maxZ;
+
+            minX = 50;
+            maxX = 350;
+            minY = 50;
+            maxY = 150;
+            minZ = 50;
+            maxZ = 200;
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+            points3d = Drawing_Service.randomPoints(bitmap3d, graphics3d, pictureBox1, Int32.Parse(textBox1.Text), minX, maxX, minY, maxY, minZ, maxZ, new PointF(pictureBox1.Size.Width/2, pictureBox1.Size.Height/2), camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                points2d.Add(points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2)));
+            }
+            Drawing_Service.drawPoints(bitmap3d, graphics3d, pictureBox1, points2d);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -53,15 +72,45 @@ namespace Delaunay.Model3D
             pictureBox1.Image = null;
             bitmap3d = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics3d = Graphics.FromImage(bitmap3d);
+            //points2d = new List<PointF>();
 
             zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
             cube.xRotation(15);
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                points3d[i].xRotation(15);
+            }
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
 
             cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                PointF temp = points2d[i];
+                points2d[i] = points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                //points2d.Add(points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2)));
+                foreach (var triangle in trianglesXY)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesYZ)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesZX)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+            }
+
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesXY);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesYZ);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesZX);
 
             bitmap3d = Drawing_Service.drawingCube(cube, graphics3d, bitmap3d);
+            Drawing_Service.drawPoints(bitmap3d, graphics3d, pictureBox1, points2d);
             pictureBox1.Image = bitmap3d;
             pictureBox1.Refresh();
         }
@@ -71,15 +120,45 @@ namespace Delaunay.Model3D
             pictureBox1.Image = null;
             bitmap3d = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics3d = Graphics.FromImage(bitmap3d);
+            //points2d = new List<PointF>();
 
             zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
             cube.yRotation(15);
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                points3d[i].yRotation(15);
+            }
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
 
             cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                PointF temp = points2d[i];
+                points2d[i] = points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                //points2d.Add(points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2)));
+                foreach (var triangle in trianglesXY)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesYZ)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesZX)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+            }
+
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesXY);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesYZ);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesZX);
 
             bitmap3d = Drawing_Service.drawingCube(cube, graphics3d, bitmap3d);
+            Drawing_Service.drawPoints(bitmap3d, graphics3d, pictureBox1, points2d);
             pictureBox1.Image = bitmap3d;
             pictureBox1.Refresh();
         }
@@ -89,17 +168,115 @@ namespace Delaunay.Model3D
             pictureBox1.Image = null;
             bitmap3d = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
             graphics3d = Graphics.FromImage(bitmap3d);
+            //points2d = new List<PointF>();
 
             zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
             cube.zRotation(15);
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                points3d[i].zRotation(15);
+            }
 
             Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
 
             cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+            for (int i = 0; i < points3d.Count; i++)
+            {
+                PointF temp = points2d[i];
+                points2d[i] = points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                //points2d.Add(points3d[i].Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2)));
+                foreach (var triangle in trianglesXY)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesYZ)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+
+                foreach (var triangle in trianglesZX)
+                {
+                    triangle.convert(temp, points2d[i]);
+                }
+            }
+
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesXY);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesYZ);
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesZX);
 
             bitmap3d = Drawing_Service.drawingCube(cube, graphics3d, bitmap3d);
+            Drawing_Service.drawPoints(bitmap3d, graphics3d, pictureBox1, points2d);
             pictureBox1.Image = bitmap3d;
             pictureBox1.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bitmap3d = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graphics3d = Graphics.FromImage(bitmap3d);
+            textBox1.Text = null;
+
+            zoom = (float)Screen.PrimaryScreen.Bounds.Width / 1.5f;
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+            cube = new Cube();
+            cube.InitializeCube(400, 200, 250);
+            cube.count2D(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+            pictureBox1.Image = Drawing_Service.drawingCube(cube, graphics3d, bitmap3d);
+
+            points2d = null;
+            points3d = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Camera camera = new Camera(cube.center.x, cube.center.z, ((cube.center.x * zoom) / cube.center.x));
+
+            List<PointF> pointsXY = new List<PointF>();
+            List<PointF> pointsYZ = new List<PointF>();
+            List<PointF> pointsZX = new List<PointF>();
+            foreach (var point in points3d)
+            {
+                //xy axis
+                Vector3D vector3D = new Vector3D(point.x, point.y, 0);
+                //PointF pointXY = vector3D.Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                PointF pointXY = new PointF(point.x, point.y);
+                pointsXY.Add(pointXY);
+
+                //yz axis
+                vector3D = new Vector3D(point.y, point.z, 0);
+                //PointF pointYZ = vector3D.Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                PointF pointYZ = new PointF(point.y, point.z);
+                pointsYZ.Add(pointYZ);
+
+                //zx axis
+                vector3D = new Vector3D(point.z, point.x, 0);
+                //PointF pointZX = vector3D.Count2d(camera, zoom, new PointF(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2));
+                PointF pointZX = new PointF(point.z, point.x);
+                pointsZX.Add(pointZX);
+            }
+
+            trianglesXY = Delaunay_Service.BowyerWatsonAlgorithm(pointsXY);
+            foreach (var triangle in trianglesXY)
+            {
+                triangle.convertXY(points2d, points3d);
+            }
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesXY);
+
+            trianglesYZ = Delaunay_Service.BowyerWatsonAlgorithm(pointsYZ);
+            foreach (var triangle in trianglesYZ)
+            {
+                triangle.convertYZ(points2d, points3d);
+            }
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesYZ);
+
+            trianglesZX = Delaunay_Service.BowyerWatsonAlgorithm(pointsZX);
+            foreach (var triangle in trianglesZX)
+            {
+                triangle.convertZX(points2d, points3d);
+            }
+            Draw_Service.drawTriangle(bitmap3d, graphics3d, pictureBox1, trianglesZX);
         }
     }
 }
