6eec0df2a140d89e55ac8673064b18baa1ede7cb
sehta7@gmail.com <sehta7@gmail.com>
2018-11-27 20:17:20 +0100

    add geometry to check if point is inside sfere made by four points (in 3d)

---
+++ Delaunay/Delaunay/Model3D/Geometry.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delaunay.Model3D
+{
+    class Geometry
+    {
+        public static bool inSphere(Vector3D a, Vector3D b, Vector3D c, Vector3D d, Vector3D pointToCheck)
+        {
+            bool inside = false;
+
+            float t1 = countSquares(a.x, a.y, a.z);
+            float t2 = countSquares(b.x, b.y, b.z);
+            float t3 = countSquares(c.x, c.y, c.z);
+            float t4 = countSquares(d.x, d.y, d.z);
+
+            float T = Convert.ToSingle(Matrix.countMatrix4x4(a.x, a.y, a.z, 1,
+                                                            b.x, b.y, b.z, 1,
+                                                            c.x, c.y, c.z, 1,
+                                                            d.x, d.y, d.z, 1));
+            float D = Convert.ToSingle(Matrix.countMatrix4x4(t1, a.y, a.z, 1,
+                                                            t2, b.y, b.z, 1,
+                                                            t3, c.y, c.z, 1,
+                                                            t4, d.y, d.z, 1)) / T;
+            float E = Convert.ToSingle(Matrix.countMatrix4x4(a.x, t1, a.z, 1,
+                                                            b.x, t2, b.z, 1,
+                                                            c.x, t3, c.z, 1,
+                                                            d.x, t4, d.z, 1)) / T;
+            float F = Convert.ToSingle(Matrix.countMatrix4x4(a.x, a.y, t1, 1,
+                                                            b.x, b.y, t2, 1,
+                                                            c.x, c.y, t3, 1,
+                                                            d.x, d.y, t4, 1)) / T;
+            float G = Convert.ToSingle(Matrix.countMatrix4x4(a.x, a.y, a.z, t1,
+                                                            b.x, b.y, b.z, t2,
+                                                            c.x, c.y, c.z, t3,
+                                                            d.x, d.y, d.z, t4)) / T;
+            Vector3D center = countCenter(D, E, F);
+            float r = Radius(D, E, F, G);
+
+            double pointCenterLength = countLength(center, pointToCheck);
+
+            if (pointCenterLength < r)
+            {
+                inside = true;
+            }
+
+            return inside;
+        }
+
+        private static double countLength(Vector3D p1, Vector3D p2)
+        {
+            return Math.Sqrt(Math.Pow((p1.x - p2.x), 2) + Math.Pow((p1.y - p2.y), 2) + Math.Pow((p1.z - p2.z), 2));
+        }
+
+        private static Vector3D countCenter(float D, float E, float F)
+        {
+            return new Vector3D((-D / 2), (-E / 2), (-F / 2));
+        }
+
+        private static float Radius(float D, float E, float F, float G)
+        {
+            return Convert.ToSingle((Math.Sqrt(Math.Pow(D, 2) + Math.Pow(E, 2) + Math.Pow(F, 2) - 4 * G)) / 2);
+        }
+
+        private static float countSquares(float a, float b, float c)
+        {
+            return Convert.ToSingle(-(Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)));
+        }
+    }
+}
---
+++ Delaunay/Delaunay/Model3D/Matrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delaunay.Model3D
+{
+    class Matrix
+    {
+        public static double countMatrix4x4(double a11, double a12, double a13, double a14,
+                                    double a21, double a22, double a23, double a24,
+                                    double a31, double a32, double a33, double a34,
+                                    double a41, double a42, double a43, double a44)
+        {
+            double result = 0;
+
+            result = a11 * ((a22 * a33 * a44) + (a32 * a43 * a24) + (a42 * a23 * a34) - (a24 * a33 * a42) - (a34 * a43 * a22) - (a44 * a23 * a32))
+                    - a21 * ((a12 * a33 * a44) + (a32 * a43 * a14) + (a42 * a13 * a34) - (a14 * a33 * a42) - (a34 * a43 * a12) - (a44 * a13 * a32))
+                    + a31 * ((a12 * a23 * a44) + (a22 * a43 * a14) + (a42 * a13 * a24) - (a14 * a23 * a42) - (a24 * a43 * a12) - (a44 * a13 * a22))
+                    - a41 * ((a12 * a23 * a34) + (a22 * a33 * a14) + (a32 * a13 * a24) - (a14 * a23 * a32) - (a24 * a33 * a12) - (a34 * a13 * a22));
+
+            return result;
+        }
+    }
+}
